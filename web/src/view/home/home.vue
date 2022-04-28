a28137f09b0e54b78a779afc9b3fffdf501aab7d
luojinlong <iamloso@126.com>
2022-04-28 13:31:13 +0800

    add 育婴员

--- web/src/view/home/home.vue
+++ web/src/view/home/home.vue
@@ -8,7 +8,7 @@
       <el-main>
         <el-form ref="elForm" :model="formData" :rules="rules" size="small" label-width="100px">
           <el-scrollbar height="600px">
-            <el-form-item label="通知:" style="color: red">
+            <el-form-item label="通知:" style="color: red" v-if="false">
               <span style="float: left">系统报名时间已截止，新同学提交的报名信息后台不在审核，请知晓。已提交报名信息同学可以继续修改信息，修改图片或证明信息上传成功后不要忘记点击提交按钮，感谢配合！</span>
             </el-form-item>
             <el-form-item label="审核结果:" style="color: red" v-if="formData.verifyReason !== ''">
@@ -674,6 +674,8 @@ export default {
         'value': 2
       }, { 'label': '1+X幼儿照护',
         'value': 3
+      }, { 'label': '育婴员',
+        'value': 4
       }],
       levelOptions: [{
         'label': '2020',
